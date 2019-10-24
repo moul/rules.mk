db97b05b77d42c8b10bd3092d77d36e9f03f8a40
Manfred Touron <m@42.am>
2019-10-24 15:36:00 +0200

    chore: go test runs without verbosity by default and with a timeout of 30s

--- rules.mk
+++ rules.mk
@@ -68,6 +68,7 @@ ifdef GOPKG
 GO ?= go
 GOPATH ?= $(HOME)/go
 GO_INSTALL_OPTS ?=
+GO_TEST_OPTS ?= -test.timeout=30s
 
 ifdef GOBINS
 .PHONY: go.install
@@ -91,7 +92,7 @@ go.unittest:
 	echo "" > /tmp/coverage.txt
 	@set -e; for dir in `find . -type f -name "go.mod" | grep -v /vendor/ | sed 's@/[^/]*$$@@' | sort | uniq`; do ( set -xe; \
 	  cd $$dir; \
-	  $(GO) test -v -cover -coverprofile=/tmp/profile.out -covermode=atomic -race ./...; \
+	  $(GO) test $(GO_TEST_OPTS) -cover -coverprofile=/tmp/profile.out -covermode=atomic -race ./...; \
 	  if [ -f /tmp/profile.out ]; then \
 	    cat /tmp/profile.out >> /tmp/coverage.txt; \
 	    rm -f /tmp/profile.out; \
